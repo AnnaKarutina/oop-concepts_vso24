1c8666d89d44d13032561c7c18f4802a7aa9dc6a
Anna Karutina <anna.karutina@khk.ee>
2024-11-22 15:26:03 +0200

    Ylesanne 4 - different shapes

--- inheritance.js
+++ inheritance.js
@@ -34,10 +34,112 @@ class Circle extends Shape {
     
 }
 
-const shape1 =  new Shape('red')
-console.log('shape color', shape1.getColor())
-console.log('shape area', shape1.getArea())
+class Square extends Shape {
+    #side
+    constructor(color, side){
+        super(color)
+        this.#side = side
+    }
+
+    getArea(){
+        return Math.pow(this.#side, 2)
+    }
+
+    print(){
+        return `Square (a: {${this.#side}}, color: {${this.getColor()}}).`
+    }
+    
+}
+
+class Rectangle extends Shape {
+    #length
+    #width
+    constructor(color, length, width){
+        super(color)
+        this.#length = length
+        this.#width = width
+    }
+
+    getArea(){
+        return this.#length * this.#width    
+    }
+
+    print(){
+        return `Rectangle (l: {${this.#length}}, w: {${this.#width}}, color: {${this.getColor()}}).`
+    }
+    
+}
+
+ 
+
+const shape =  new Shape('red')
+console.log('shape color', shape.getColor())
+console.log('shape area', shape.getArea())
 
 const circle1 =  new Circle('blue', 5)
 console.log(circle1.print())
 console.log('circle area', circle1.getArea())
+
+const circle2 =  new Circle('violet', 8)
+console.log(circle2.print())
+console.log('circle area', circle2.getArea())
+
+const square =  new Square('green', 10)
+console.log(square.print())
+console.log('square area', square.getArea())
+
+const rectangle =  new Rectangle('yellow', 5, 8)
+console.log(rectangle.print())
+console.log('rectangle area', rectangle.getArea())
+
+class Paint {
+
+    constructor(){
+        this.shapes =[]  
+    } 
+
+    addShape(shape){
+        this.shapes.push(shape)
+    } 
+
+    getShapes(){
+        return this.shapes
+    } 
+
+    calculateTotalArea(){
+        this.totalArea = 0
+        this.shapes.forEach(shape => {
+            this.totalArea += shape.getArea()
+        })
+        return this.totalArea 
+    } 
+
+    getCircles(){
+        this.circles = []
+        this.circles = this.shapes.filter(shape => shape.constructor.name == 'Circle')
+        return this.circles
+    }
+    
+    getSquares(){
+        this.squares = []
+        this.squares = this.shapes.filter(shape => shape instanceof 'Square')
+        return this.squares
+    }
+
+    getRectangles(){
+        this.rectangles = []
+        this.rectangles = this.shapes.filter(shape => shape instanceof 'Rectangle')
+        return this.rectangles
+    }
+}
+
+const paint = new Paint()
+
+paint.addShape(circle1)
+paint.addShape(square)
+paint.addShape(rectangle)
+paint.addShape(circle2)
+
+console.log(paint.getShapes())
+console.log(paint.calculateTotalArea())
+paint.getCircles().forEach(shape => console.log(shape.print()))
